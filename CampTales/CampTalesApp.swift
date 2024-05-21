a428574daca879dce100eda8dc2d616123706857
TrevonT <trevonteart@gmail.com>
2024-05-21 08:49:01 -0400

    Initial Commit

---
+++ CampTales/CampTalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampTalesApp.swift
+//  CampTales
+//
+//  Created by Trevon Teart on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CampTalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
